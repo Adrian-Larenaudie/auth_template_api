78cc4b8e6b95dc362f657348b66d007bc4d16978
Adrian-Larenaudie <adrian.larenaudie@gmail.com>
2023-10-30 16:33:37 +0100

    [FIX] cors config

--- config/cors.js
+++ config/cors.js
@@ -14,18 +14,7 @@ const corsConfig = cors({
         'DELETE'
     ],
 
-    allowedHeaders: `
-        Access-Control-Allow-Origin, 
-        Access-Control-Allow-Credentials, 
-        X-CSRF-Token, Origin, 
-        X-Requested-With,
-        x-access-token, 
-        role, 
-        Content, 
-        Accept,
-        Content-Type, 
-        Authorization
-    `,
+    allowedHeaders: `Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Allow-Credentials,X-CSRF-Token,X-Requested-With,x-access-token,role,Content,Accept,Content-Type,Authorization`,
 });
 
 module.exports = corsConfig;
